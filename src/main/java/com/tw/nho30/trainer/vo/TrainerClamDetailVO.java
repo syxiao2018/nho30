0840ced0b7b9a5ebc20c7af3a5535bd30fe2c641
kaliu <kaliu@thoughtworks.com>
2018-06-12 21:14:28 +0800

    训练营详情-空TrainerClamDetailVO

---
+++ src/main/java/com/tw/nho30/trainer/vo/TrainerClamDetailVO.java
@@ -0,0 +1,13 @@
+package com.tw.nho30.trainer.vo;
+
+import io.swagger.annotations.ApiModel;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@ApiModel(value = "trainerClamDetail", description = "训练营详情")
+public class TrainerClamDetailVO {
+
+
+}
