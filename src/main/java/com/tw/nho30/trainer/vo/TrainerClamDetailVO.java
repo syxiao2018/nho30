569a8cc292a96b3c9e1b935dd2a45bcba85f8d15
kaliu <kaliu@thoughtworks.com>
2018-06-12 22:58:44 +0800

    Merge branch 'master' of https://github.com/syxiao2018/nho30
    # Conflicts:
    #	src/main/java/com/tw/nho30/trainer/model/TrainerClam.java

--- src/main/java/com/tw/nho30/trainer/vo/TrainerClamDetailVO.java
+++ src/main/java/com/tw/nho30/trainer/vo/TrainerClamDetailVO.java
@@ -1,13 +1,59 @@
 package com.tw.nho30.trainer.vo;
 
+import com.tw.nho30.trainer.model.TaskCard;
 import io.swagger.annotations.ApiModel;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.List;
 
 @Getter
 @Setter
 @ApiModel(value = "trainerClamDetail", description = "训练营详情")
 public class TrainerClamDetailVO {
 
+    /**
+     * 唯一标示
+     */
+    private String id;
 
+    /**
+     * 名字
+     */
+    private String name;
+
+    /**
+     * summary
+     */
+    private String summary;
+
+    /**
+     * 开课时间
+     */
+    private String startDate;
+
+    /**
+     * 报名人数
+     */
+    private int inNum;
+
+    private List<TaskCardVO> taskCards;
+
+    @Data
+    public static class TaskCardVO {
+
+        private String id;
+
+        private String name;
+
+        private String description;
+
+        private int status;
+
+        private int score;
+
+    }
 }
+
+
