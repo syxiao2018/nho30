b6a269879baa82937588140644222c3d384eada0
zhengxiongli <xlzheng@thoughtworks.com>
2018-06-12 19:52:32 +0800

    create packages

---
+++ src/main/java/com/tw/nho30/trainer/Dao/impl/UserDaoImpl.java
@@ -0,0 +1,8 @@
+package com.tw.nho30.trainer.Dao.impl;
+
+import com.tw.nho30.trainer.Dao.UserDao;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserDaoImpl implements UserDao {
+}
--- src/main/java/com/tw/nho30/trainer/service/impl/UserServiceImpl.java
+++ src/main/java/com/tw/nho30/trainer/service/impl/UserServiceImpl.java
@@ -1,6 +1,8 @@
 package com.tw.nho30.trainer.service.impl;
 
 import com.tw.nho30.trainer.service.UserService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserServiceImpl implements UserService {
 }
